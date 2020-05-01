a0e3a02322a802f95ecb9a29947eca468f2b6ba9
Shadin <shadin710@gmail.com>
2020-05-02 02:34:06 +0600

    Added a event listener

--- views/js/chat.js
+++ views/js/chat.js
@@ -1,1 +1,13 @@
 var socket =  io.connect('http://localhost:3000');
+
+var message = document.getElementById('message');
+    btn = document.getElementById('send');
+    output= document.getElementById('output');
+    handle= document.getElementById('handle');
+
+btn.addEventListener('click',()=>{
+    socket.emit('chat',{
+        handle:handle.value,
+        message: message.value
+    });
+});
