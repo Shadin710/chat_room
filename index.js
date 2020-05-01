c6a351eb649950755c98925d3e755b6ba6a301f5
Shadin <shadin710@gmail.com>
2020-05-01 23:00:46 +0600

    Added server side socket.io

---
+++ index.js
@@ -0,0 +1,37 @@
+
+require('dotenv').config();
+const express = require('express');
+const app =  express();
+const bodyParser=  require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+const socket = require('socket.io');
+
+
+//some setting code
+app.use(express.static('views'));
+app.set('view engine','pug');
+app.set('views','./views');
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended:true}));
+//end
+
+
+
+const port = process.env.PORT;
+const server = app.listen(port,()=>{
+
+    console.log(`listening to ${port}....`)
+});
+
+
+
+app.get('/',(req,res)=>{
+    res.render('index');
+    const io= socket(server);
+
+    io.on('connection',(socket)=>{
+        console.log(`socket connected...${socket.id}`);
+    });
+});
